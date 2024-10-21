fa8283a7045051142df29840a9414346f9f34b76
EMartynova <vtb4081701@dev.vtb.ru>
2024-10-21 17:29:54 +0300

    search for an existing file

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int n = 0;
-        while (true) {
+        while (true){
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
-            if (isDirectory==true) {
+            if (isDirectory == true) {
+                System.out.println("Путь указан неверно");continue;
+            }
+            if (fileExists == true) {
+                System.out.println("Путь указан верно");
+                n = n + 1;
+                System.out.println("Это файл номер " + n);
+            } else {
                 System.out.println("Путь указан неверно");
             }
-            if (fileExists == true /*&& isDirectory == true*/) {
-                System.out.println("Путь указан верно");
-                n = n+1;
-                System.out.println("Это файл номер " + n);
-            }
-//            else {
-//                System.out.println("Путь указан неверно");
-//            }
-
         }
     }
 }
